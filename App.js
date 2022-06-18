d4e460c7a8a1225f027a0d44a4fdacc6e0601534
Mukiibi-G-J <mukiibijosephgilbert865@gmail.com>
2022-06-18 15:31:44 +0300

    adding Google maps and changing color

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
 
-export default function App() {
+import HomeScreen from './src/Screens/HomeScreen';
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      {/* <View styles={styles.container}> */}
+
+      <HomeScreen />
+    </>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
